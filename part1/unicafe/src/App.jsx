6be37871e38673e7f1efcc7c1019e4b5f0227b36
Darius Tan <dariusTan94@gmail.com>
2025-04-07 21:11:52 +0800

    [PART 1 EXERCISE 1.8] Unicafe step 3

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,10 @@
 import { useState } from 'react'
 
+//Common
+const goodLabel = 'good'
+const neutralLabel = 'neutral'
+const badLabel = 'bad'
+
 const FeedbackButton = ({onClick, label}) => {
   return <button onClick={onClick}>{label}</button>
 }
@@ -8,9 +13,9 @@ const FeedbackCounter = ({label, counter}) => {
   return <p>{label} {counter}</p>
 }
 
-const Average = ({good, neutral, bad}) => {
-  const total = good + neutral + bad
-  const average = (good - bad) / total
+const Average = ({counter}) => {
+  const total = counter.goodCounter + counter.neutralCounter + counter.badCounter
+  const average = (counter.goodCounter - counter.badCounter) / total
   
   if(Number.isNaN(average)) {
     return <p>average 0</p>
@@ -19,9 +24,9 @@ const Average = ({good, neutral, bad}) => {
   return <p>average {average}</p>
 }
 
-const PositivePercentage = ({good, neutral, bad}) => {
-  const total = good + neutral + bad
-  const positivePercentage = (good / total) * 100
+const PositivePercentage = ({counter}) => {
+  const total = counter.goodCounter + counter.neutralCounter + counter.badCounter
+  const positivePercentage = (counter.goodCounter / total) * 100
 
   if(Number.isNaN(positivePercentage)) {
     return <p>positive 0</p>
@@ -30,15 +35,25 @@ const PositivePercentage = ({good, neutral, bad}) => {
   return <p>positive {positivePercentage}</p>
 }
 
+const Statistics = ({counter}) => {
+  return (
+    <>
+      <p>Statistics</p>
+      <FeedbackCounter label={goodLabel} counter={counter.goodCounter} />
+      <FeedbackCounter label={neutralLabel} counter={counter.neutralCounter} />
+      <FeedbackCounter label={badLabel} counter={counter.badCounter} />
+      <Average counter={counter} />
+      <PositivePercentage counter={counter} />
+    </>
+
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
-  const goodLabel = 'good'
-  const neutralLabel = 'neutral'
-  const badLabel = 'bad'
 
   const handleGoodFeedback = () => {
     setGood(good + 1)
@@ -52,18 +67,19 @@ const App = () => {
     setBad(bad + 1)
   }
 
+  const counters = {
+    goodCounter: good,
+    neutralCounter: neutral,
+    badCounter: bad
+  }
+
   return (
     <div>
       <p>Give feedback</p>
       <FeedbackButton onClick={handleGoodFeedback} label={goodLabel} />
       <FeedbackButton onClick={handleNeutralFeedback} label={neutralLabel} />
       <FeedbackButton onClick={handleBadFeedback} label={badLabel} />
-      <p>Statistics</p>
-      <FeedbackCounter label={goodLabel} counter={good} />
-      <FeedbackCounter label={neutralLabel} counter={neutral} />
-      <FeedbackCounter label={badLabel} counter={bad} />
-      <Average good={good} neutral={neutral} bad={bad} />
-      <PositivePercentage good={good} neutral={neutral} bad={bad} />
+      <Statistics counter={counters}/>
     </div>
   )
 }
